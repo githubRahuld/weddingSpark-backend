110dd4f9c8193d4f2eecaf436d28bdd7b63f996e
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-10-16 21:09:30 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import { app } from "./app.js"; //! imp to check this
-const port = process.env.PORT;
 
-dotenv.config({ path: "/env" });
+dotenv.config({
+    path: "./.env",
+});
 
 connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`SERVER is listening on PORT : ${process.env.PORT}`);
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on PORT ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed! ", err);
     });
-  })
-  .catch((error) => {
-    console.log("MONGODB connection failed", error);
-  });
